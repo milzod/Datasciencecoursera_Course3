2643ac2a4d33f4a0197ee802a8ecd7ffe81bddc2
milzod <milzod@gmail.com>
2014-07-24 12:09:42 -0600

    Removed hard coded file path
    Removed hardcoded file file and added getwd() so that it can run as
    long as the data is in the same location

--- run_analysis.R
+++ run_analysis.R
@@ -1,14 +1,14 @@
 library(reshape2)
 # Part 1: Merges the training and the test sets to create one data set
 # 1.1: Load X, Y and subject training data files 
-Xtraining <- read.csv("/Users/mzodge/Workspaces/R/UCI HAR Dataset/train/X_train.txt", sep = "", header = FALSE)
-Ytraining <- read.csv("/Users/mzodge/Workspaces/R/UCI HAR Dataset/train/y_train.txt", sep = "", header = FALSE)
-Subtraining <- read.csv("/Users/mzodge/Workspaces/R/UCI HAR Dataset/train/subject_train.txt", sep = " ", header = FALSE)
+Xtraining <- read.csv(paste0(getwd(),"/", "UCI HAR Dataset/train/X_train.txt"), sep = "", header = FALSE)
+Ytraining <- read.csv(paste0(getwd(),"/", "UCI HAR Dataset/train/y_train.txt"), sep = "", header = FALSE)
+Subtraining <- read.csv(paste0(getwd(),"/", "UCI HAR Dataset/train/subject_train.txt"), sep = " ", header = FALSE)
 
 # 1.2: Load X, Y and subject test data files 
-Xtest <- read.csv("/Users/mzodge/Workspaces/R/UCI HAR Dataset/test/X_test.txt", sep = "", header = FALSE)
-Ytest <- read.csv("/Users/mzodge/Workspaces/R/UCI HAR Dataset/test/y_test.txt", sep = "", header = FALSE)
-Subtest <- read.csv("/Users/mzodge/Workspaces/R/UCI HAR Dataset/test/subject_test.txt", sep = " ", header = FALSE)
+Xtest <- read.csv(paste0(getwd(),"/", "UCI HAR Dataset/test/X_test.txt"), sep = "", header = FALSE)
+Ytest <- read.csv(paste0(getwd(),"/", "UCI HAR Dataset/test/y_test.txt"), sep = "", header = FALSE)
+Subtest <- read.csv(paste0(getwd(),"/", "UCI HAR Dataset/test/subject_test.txt"), sep = " ", header = FALSE)
 
 # 1.3: Merge training and test data sets
 test <- cbind(Xtest, Ytest, Subtest)
@@ -18,8 +18,8 @@ mz <- rbind(test, training)
 # Doing part 4 first for easier operation
 # Part 4: Appropriately labels the data set with descriptive variable names
 # 4.1: Read features and activities data file
-features <- read.csv("/Users/mzodge/Workspaces/R/UCI HAR Dataset/features.txt", sep = " ", header = FALSE)
-activities <- read.csv("/Users/mzodge/Workspaces/R/UCI HAR Dataset/activity_labels.txt", sep = " ", header = FALSE)
+features <- read.csv("UCI HAR Dataset/features.txt", sep = " ", header = FALSE)
+activities <- read.csv("UCI HAR Dataset/activity_labels.txt", sep = " ", header = FALSE)
 
 # 4.2: Remove any special characters
 features <- gsub("-", ".", gsub("[\\(\\)]", "", as.character(features$V2)))
